3218fc9ecbdbeade6df9b08be22c7398f3e53cde
predat <sylvain@predat.fr>
2016-02-26 14:31:24 +0100

    bump version

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
 
 setup(
     name='wetransferpy',
-    version='0.1',
+    version='0.1.1',
     description='WeTransfer',
     long_description=long_description,
     url='https://github.com/creemerica/wetransfer-upload',
