b890526d2a4647dca432bb9082fbc07e618a94fa
predat <sylvain@predat.fr>
2016-12-26 14:18:43 +0100

    adapt wetransferpy to the new wetransfer API

--- wetransferpy/wetransfer.py
+++ wetransferpy/wetransfer.py
@@ -14,19 +14,46 @@ from StringIO import StringIO
 import requests
 from requests_toolbelt import MultipartEncoder, MultipartEncoderMonitor
 
+import Queue
+import threading
+
 reload(sys)
 sys.setdefaultencoding('utf8')
 
-WETRANSFER_API_URL = "https://www.wetransfer.com/api/ui/transfers"
+WETRANSFER_API_URL = "https://wetransfer.com/api/ui/transfers"
 WETRANSFER_LINK_URL = "{}/link".format(WETRANSFER_API_URL)
 WETRANSFER_EMAIL_URL = "{}/email".format(WETRANSFER_API_URL)
 WETRANSFER_TRANSFER_URL = "{api_url}/{transfer_id}/files"
 WETRANSFER_FILE_URL = "{api_url}/{transfer_id}/files/{file_id}"
+WETRANSFER_TRANSFER_FINALIZE_URL = "{api_url}/{transfer_id}/finalize"
+WETRANSFER_FILE_FINALIZE_URL = "{api_url}/{transfer_id}/files/{file_id}/finalize"
 
 DOWNLOAD_URL_PARAMS_PREFIX = "downloads/"
 CHUNK_SIZE_U = 5242880
+#CHUNK_SIZE_U = 2621440
 CHUNK_SIZE_D = 1024
 
+
+class ThreadUploader(threading.Thread):
+    def __init__(self, queue):
+        threading.Thread.__init__(self)
+        self.queue = queue
+
+    def run(self):
+        while True:
+            params = self.queue.get()
+
+            logging.info("chunk_number: %s" % params[4])
+            url = params[0]["template"]["formdata"]["action"]
+            logging.info("################ url: %s" % url)
+            dataChunkUpload = collections.OrderedDict()
+            for k, v in params[0]["template"]["form"].items():
+                dataChunkUpload[k] = v
+            dataChunkUpload["file"] = (params[1], params[2],params[3])
+            e = MultipartEncoder(fields=dataChunkUpload)
+            r = requests.post(url, data=e, headers={'Content-Type': e.content_type})
+
+            self.queue.task_done()
 
 class WeTransfer(object):
 
@@ -76,60 +103,56 @@ class WeTransfer(object):
         r = self.session.post("https://www.wetransfer.com/users/sign_in",
                               data=dataLogin)
 
-    def _get_transfer_id(self):
+    def _get_transfer_id(self, filenames):
         dataTransferId = {
-            "channel": self.channel,
-            "expire_in": self.expire_in,
-            "from": self.sender,
+            "filenames": [filenames],
             "message": self.message,
-            "pw": self.deliverypassword,
-            "to[]": self.receivers,
-            "utype": "js"}
-        if self.sender:
-            dataTransferId["ttype"] = "1"
-        else:
-            dataTransferId["ttype"] = "4"
-        r = self.session.post(WE_TRANSFER_API_URL, data=dataTransferId)
+        }
 
+        if self.sender:
+            dataTransferId["from"] = self.sender
+            dataTransferId["recipients"] = self.receivers
+            url = WETRANSFER_EMAIL_URL
+        else:
+            url = WETRANSFER_LINK_URL
+
+        r = self.session.post(url, json=dataTransferId)
         while(r.status_code != 200):
             time.sleep(2)
-            r = self.session.post(WE_TRANSFER_API_URL, data=dataTransferId)
+            r = self.session.post(url, json=dataTransferId)
 
         response_data = json.loads(r.content)
-
         try:
-            return response_data["transfer_id"]
+            return response_data["id"]
         except KeyError as e:
             raise Exception('Error', e)
 
     def _get_file_objectid(self, filename, filesize):
-        dataFileObjectId = {
-            "chunked": "true",
-            "direct":   "false",
-            "filename":    filename,
-            "filesize": filesize}
-
+        logging.debug("-------------------------------- _get_file_objectid")
+        dataFileObjectId = {"name": filename, "size": filesize}
         r = self.session.post(
-            (WE_TRANSFER_API_URL + "/{0}/file_objects")
-            .format(self.transfer_id),
-            data=dataFileObjectId)
+            WETRANSFER_TRANSFER_URL.format(api_url=WETRANSFER_API_URL,
+                                           transfer_id=self.transfer_id),
+            json=dataFileObjectId)
         response_data = json.loads(r.content)
-
         return response_data
 
     def _get_chunk_info_for_upload(self, fileObjectId, chunkNumber,
                                    chunkSize=CHUNK_SIZE_U):
+        logging.info("-------------------------------- _get_chunk_info_for_upload")
         dataChunk = {
-            "chunkNumber": chunkNumber,
-            "chunkSize":  chunkSize,
-            "retries": "0"}
-
+            "chunk_number": chunkNumber,
+            "chunk_size":  chunkSize,
+            "retries": 0}
+        logging.info(dataChunk)
         r = self.session.put(
-            (WE_TRANSFER_API_URL + "/{0}/file_objects/{1}")
-            .format(self.transfer_id, fileObjectId),
-            data=dataChunk)
-
-        return json.loads(r.content)
+            WETRANSFER_FILE_URL.format(api_url=WETRANSFER_API_URL,
+                                       transfer_id=self.transfer_id,
+                                       file_id=fileObjectId),
+            json=dataChunk)
+        response_data = json.loads(r.content)
+        logging.debug(response_data)
+        return response_data
 
     def _draw_progressbar(self, percent, barLen=40):
         progress = ""
@@ -149,14 +172,13 @@ class WeTransfer(object):
 
     def _upload_chunk(self, chunkInfo, filename, dataBin, fileType,
                       chunkNumber, fileSize):
-        url = chunkInfo["url"]
-
+        logging.info("-------------------------------- _upload_chunk")
+        url = chunkInfo["template"]["formdata"]["action"]
         dataChunkUpload = collections.OrderedDict()
-        for k, v in chunkInfo["fields"].items():
+        for k, v in chunkInfo["template"]["form"].items():
             dataChunkUpload[k] = v
 
         dataChunkUpload["file"] = (filename, dataBin, fileType)
-
         e = MultipartEncoder(fields=dataChunkUpload)
         if self.progress:
             e = MultipartEncoderMonitor(e,
@@ -167,16 +189,24 @@ class WeTransfer(object):
                               headers={'Content-Type': e.content_type})
 
     def _finalize_chunks(self, fileObjectId, partCount):
-        dataFinalizeChunk = {
-            "finalize_chunked": "true",
-            "part_count": partCount}
-        r = self.session.put((WE_TRANSFER_API_URL +
-            "/{0}/file_objects/{1}").format(self.transfer_id, fileObjectId),
-                             data=dataFinalizeChunk)
+        logging.info("-------------------------------- _finalize_chunks")
+        dataFinalizeChunk = { "chunk_count": partCount }
+        r = self.session.put(
+            WETRANSFER_FILE_FINALIZE_URL.format(
+                api_url=WETRANSFER_API_URL,
+                transfer_id=self.transfer_id,
+                file_id=fileObjectId
+            ),
+            json=dataFinalizeChunk
+        )
 
     def _finalize_transfer(self):
-        r = self.session.put((WE_TRANSFER_API_URL +
-            "/{0}/finalize").format(self.transfer_id))
+        r = self.session.put(
+            WETRANSFER_TRANSFER_FINALIZE_URL.format(
+                api_url=WETRANSFER_API_URL,
+                transfer_id=self.transfer_id
+            )
+        )
         response = json.loads(r.content)
         url = response["shortened_url"]
         return url
@@ -196,35 +226,42 @@ class WeTransfer(object):
             yield data
 
     def uploadFile(self, fileToUpload):
-        self.transfer_id = self._get_transfer_id()
+        fileSize = os.path.getsize(fileToUpload)
+        fileName = os.path.basename(fileToUpload)
+        self.transfer_id = self._get_transfer_id(fileName)
         logging.info("Transfer_id: %s" % self.transfer_id)
 
-        logging.debug("Upload file: %s" % fileToUpload) 
+        logging.debug("Upload file: %s" % fileToUpload)
         with open(fileToUpload, 'rb') as f:
             fileMimeType = "application/octet-stream"
-            fileSize = os.path.getsize(fileToUpload)
-            fileName = os.path.basename(fileToUpload)
 
             dataFileObjectId = self._get_file_objectid(fileName, fileSize)
-            if "url" in dataFileObjectId:
-                self._upload_chunk(dataFileObjectId, fileName,
-                                   f.read(fileSize), fileMimeType, 0, fileSize)
-                self._finalize_chunks(dataFileObjectId["file_object_id"], 1)
-            else:
-                chunkNumber = 1
+            logging.debug(dataFileObjectId)
 
-                for piece in self._read_in_chunks(f):
-                    chunkInfo = self._get_chunk_info_for_upload(
-                        dataFileObjectId["file_object_id"],
-                        chunkNumber,
-                        sys.getsizeof(piece))
-                    self._upload_chunk(chunkInfo, fileName, piece,
-                                       fileMimeType, chunkNumber-1, fileSize)
-                    chunkNumber = chunkNumber + 1
+            chunkNumber = 1
 
-                self._finalize_chunks(
-                    dataFileObjectId["file_object_id"], chunkNumber - 1)
-        print '\n'
+
+            queue = Queue.Queue()
+            for i in range(8):
+                t = ThreadUploader(queue)
+                t.setDaemon(True)
+                t.start()
+
+
+            for piece in self._read_in_chunks(f):
+                chunkInfo = self._get_chunk_info_for_upload(
+                    dataFileObjectId["id"],
+                    chunkNumber,
+                    sys.getsizeof(piece))
+                #self._upload_chunk(chunkInfo, fileName, piece,
+                #                   fileMimeType, chunkNumber-1, fileSize)
+                queue.put([chunkInfo, fileName, piece, fileMimeType, chunkNumber-1, fileSize])
+                chunkNumber = chunkNumber + 1
+
+            queue.join()
+
+            self._finalize_chunks(dataFileObjectId["id"], chunkNumber - 1)
+        #print '\n'
         return self._finalize_transfer()
 
     def uploadDir(self, top, recursive=False):
